a72cae2de24ab1a3264885b437ce2d750a3ce1d4
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-10-13 12:36:30 -0300

    animções com lib teenmax

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 import { Cards } from "./Card.js";
+import { Icones } from "./Icones.js"
 
 // no carragemento da janela chamamos a instância do cards
 window.onload = () => {
     let animaCards = new Cards();
+    let animaIcones =  new Icones(); 
+
     document.addEventListener('scroll', animaCards.scrollCards.bind(animaCards))
+
+    animaIcones.animaIcones()
 }
